19a699e1918bcc2eb58e93a13e1b6284807dd59e
Kyle Fitzpatrick <x15754001@student.ncirl.ie>
2018-02-02 12:27:54 +0000

    First Commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,35 @@
+/*
+Kyle Fitzpatrick
+22/01/18
+My First Code
+*/
+
+// Step 1: Class Signiture - Creating an Application called MyFirstProgram
+// CTRL+M brings you to the representing bracket
+public class MyFirstProgram{
+	// Main method - Engine of Program
+	// Run the application
+	public static void main (String args []){
+
+		// print something to the screen - statement
+		// ln prints to a new line
+		System.out.println("I Love Java");
+		System.out.println("I Love Monday");
+		System.out.println("I Love Programming");
+
+// Main Errors:
+// Cannot find symbol = Spelling Mistake
+// Illegal Start of expression = left out a bracket
+
+
+
+
+
+
+}// Closes main method
+
+
+
+
+
+} // closes class signature
